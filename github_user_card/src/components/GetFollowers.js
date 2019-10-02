2805b34bdaf102c391214102a78962c97b1afcff
dbriksza <dbriksza@gmail.com>
2019-10-02 18:38:58 -0400

    just had to ask nicely

--- github_user_card/src/components/GetFollowers.js
+++ github_user_card/src/components/GetFollowers.js
@@ -4,25 +4,38 @@ import axios from "axios";
 
 class GetFollowers extends React.Component {
   state = {
-    follower: []
+    follower: [],
+    followerinfo: []
   };
-  componentDidMount() {
+  componentWillMount() {
     axios
       .get("https://api.github.com/users/dbriksza/followers")
       .then(response => {
-        console.log(response.data);
+        console.log("first axios", response.data);
+        console.log("somethingdon'tmaater", response.data);
         this.setState({
-          follower: response.data
+          follower: response.data.map(el => el.login)
         });
-      })
-      .catch(err => console.log(err));
+        {
+          this.state.follower.map(follower =>
+            axios
+              .get(`https://api.github.com/users/${follower}`)
+              .then(response => {
+                this.setState({
+                  followerinfo: [...this.state.followerinfo, response.data]
+                });
+              })
+          );
+        }
+      });
   }
 
   render() {
+    console.log(this.state.followerinfo);
     return (
       <>
-        {this.state.follower.map(follower => (
-          <UserCard data={follower} />
+        {this.state.followerinfo.map(please => (
+          <UserCard data={please} />
         ))}
       </>
     );
